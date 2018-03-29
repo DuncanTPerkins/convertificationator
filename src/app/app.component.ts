3018884a13fb8ad951ed07fb829c218d8a92ae51
Duncan Perkins <duncan.perkins@claytonhomes.com>
2018-03-29 16:17:21 -0400

    proof of concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  inValue: number;
+  kil: number;
+  cel: number;
+
+  toKm(miles: number) {
+    return miles/0.62137
+  }
+
+  toCelcius(f: number) {
+    return (f-32)/1.8
+  }
+
+  evaluateConversions(value: number) {
+    this.kil = this.toKm(value);
+    this.cel = this.toCelcius(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule, MatIconModule, MatFormFieldModule, MatCardModule, MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
